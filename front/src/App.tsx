d90842b851d0ddf1f69b3e76bad49a2d53f9523b
JustLenard <justlenard.justme@gmail.com>
2022-10-28 07:23:46 +0300

    sockt .io code

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,8 +1,18 @@
 import UserCard from './components/UserCard'
+import io, { Socket } from 'socket.io-client'
+
+const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io('http://localhost:3001')
 
 interface Props {}
 
 const App: React.FC<Props> = () => {
+	const sendMessage = () => {
+		socket.emit(' hey')
+	}
+
+	io.on('connection', (socket) => {
+		console.log(`user connect with ${socket.id}`)
+	})
 	return (
 		<>
 			<input placeholder="Somethings" />
