1f77ba91d6b6588905239fabfdf9bf50901befba
JustLenard <justlenard.justme@gmail.com>
2023-04-09 15:46:41 +0300

    Connected open-ai api

--- server/src/modules/open-ai/open-ai.controller.ts
+++ server/src/modules/open-ai/open-ai.controller.ts
@@ -1,24 +1,17 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { OpenAiService } from './open-ai.service';
-import { CreateOpenAiDto } from './dto/create-open-ai.dto';
-import { UpdateOpenAiDto } from './dto/update-open-ai.dto';
 
 @Controller('open-ai')
 export class OpenAiController {
   constructor(private readonly openAiService: OpenAiService) {}
 
   @Post()
-  async getResponse(@Body('input') input: string): Promise<string> {
-    console.log(input);
-    const response = await this.openAiService.generateText(input);
+  async getResponse(@Body('message') message: string): Promise<any> {
+    console.log(message);
+    const response = await this.openAiService.getModelAnswer(message);
+    // const response = 'f';
+
+    console.log('This is response', response);
     return response;
   }
 }
--- server/src/modules/open-ai/open-ai.service.ts
+++ server/src/modules/open-ai/open-ai.service.ts
@@ -1,21 +1,49 @@
 import { Injectable } from '@nestjs/common';
 import { CreateOpenAiDto } from './dto/create-open-ai.dto';
 import { UpdateOpenAiDto } from './dto/update-open-ai.dto';
-import { OpenAIApi } from 'openai';
+import { Configuration, CreateCompletionRequest, OpenAIApi } from 'openai';
+import axios from 'axios';
+
+const DEFAULT_MODEL_ID = 'text-davinci-003';
+const DEFAULT_TEMPERATURE = 0.9;
 
 @Injectable()
 export class OpenAiService {
-  private openai: OpenAIApi;
+  private openAiApi: OpenAIApi;
 
-  // constructor() {
-  //   this.openai = new OpenAIApi({ apiKey: process.env.OPENAI_API_KEY });
-  // }
+  private readonly apiUrl = 'https://api.openai.com/v1/';
+
+  constructor() {
+    const configuration = new Configuration({
+      organization: process.env.ORGANIAZATION_ID,
+      apiKey: process.env.OPENAI_API_KEY,
+    });
+    this.openAiApi = new OpenAIApi(configuration);
+  }
+
+  async getModelAnswer(question: string, temperature?: number) {
+    try {
+      const params: CreateCompletionRequest = {
+        model: DEFAULT_MODEL_ID,
+        prompt: question,
+        temperature: temperature ? temperature : DEFAULT_TEMPERATURE,
+      };
+
+      const response = await this.openAiApi.createCompletion(params);
+
+      console.log('This is response', response);
+
+      return response.data;
+    } catch (e) {
+      console.log('This is e', e);
+    }
+  }
 
   async generateText(prompt: string): Promise<string> {
-    console.log(
-      'This is process.env.OPENAI_API_KEY',
-      process.env.OPENAI_API_KEY,
-    );
+    // console.log(
+    //   'This is process.env.OPENAI_API_KEY',
+    //   process.env.OPENAI_API_KEY,
+    // );
     // const response = await this.openai.createCompletion({
     //   model: 'text-davinci-002',
     //   prompt,
@@ -24,6 +52,30 @@ export class OpenAiService {
     //   n: 1,
     //   stop: '\n',
     // });
+    // console.log('This is response', response);
+    // console.log(
+    //   'This is process.env.OPENAI_API_KEY',
+    //   process.env.OPENAI_API_KEY,
+    // );
+
+    const response = await axios.post(
+      `${this.apiUrl}engines/davinci-codex/completions`,
+      {
+        prompt,
+        max_tokens: 1024,
+        temperature: 0.5,
+        n: 1,
+        stop: '\n',
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+        },
+      },
+    );
+
+    console.log('This is response', response);
 
     // return response.choices[0].text.trim();
     return 'hey';
